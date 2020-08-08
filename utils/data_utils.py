39417b5cc6d6a964f4f5dd00b3c1f9c132e8b77e
Abhinav Grover <abhinav.grover@robotics.utias.utoronto.ca>
2020-08-08 17:18:09 -0400

    [dataloader]: added the slip validity logic (untested)

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -10,6 +10,14 @@ Developed at UTIAS, Toronto.
 author: Abhinav Grover
 """
 
+##################### Error printing
+from __future__ import print_function
+import sys
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+#####################
+
 # Standard Imports
 import os
 import matplotlib.pyplot as plt
@@ -17,9 +25,11 @@ import numpy as np
 import math
 import scipy.io as sio
 
-SPEED_THRESH = 0.5 # TODO tune this parameter
+# GLOBAL VARIABLES
+SPEED_THRESH = 0.5              # TODO: tune this parameter
+SLIP_STD_VALID_MULTIPLIER = 2 # TODO: tune this parameter
 
-class takktile_dataloader():
+class takktile_dataloader(object):
     """
         Takktile data loading class (.mat files)
 
@@ -41,7 +51,7 @@ class takktile_dataloader():
         self.__load_data_dir(data_dir, mat_format)
 
         if self.empty():
-            print("ERROR: Not enough data in directory: {}".format(data_dir))
+            eprint("ERROR: Not enough data in directory: {}".format(data_dir))
             return
 
         # Create histogram
@@ -60,10 +70,10 @@ class takktile_dataloader():
             location: address including the name of the saved image
         """
         if self.empty():
-            print("ERROR: Cannot save histogram: empty loader")
+            eprint("ERROR: Cannot save histogram: empty loader")
             return
         if not self.create_hist:
-            print("ERROR: Cannot save histogram: histogram disabled")
+            eprint("ERROR: Cannot save histogram: histogram disabled")
             return
         self.slip_hist.savefig(location, dpi=self.slip_hist.dpi)
 
@@ -75,74 +85,88 @@ class takktile_dataloader():
     def empty(self):
         return self.__data['num'] <= self.series_len
 
-    def get_slip_data(self):
+    def get_slip_idx(self):
         """
         Return slip only data
-            validFormat: [pressure], ([slip_dir], [slip_speed])
         """
         if self.empty():
             return None
-        return self.__get_pressure(self.slip_idx), \
-               (self.__get_slip_dir(self.slip_idx), \
-               self.__get_slip_speed(self.slip_idx))
+        return self.slip_idx
 
-    def get_no_slip_data(self):
+    def get_no_slip_idx(self):
         """
         Return no slip data
-            validFormat: [pressure], ([slip_dir], [slip_speed])
         """
         if self.empty():
             return None
-        return self.__get_pressure(self.no_slip_idx), \
-               (self.__get_slip_dir(self.no_slip_idx), \
-               self.__get_slip_speed(self.no_slip_idx))
+        return self.no_slip_idx
 
-    def get_slip_stream_data(self):
+    def get_slip_stream_idx(self):
         """
         Return slip only data
-            validFormat: [pressure], ([slip_dir], [slip_speed])
         """
         if self.empty():
             return None
-        return self.__get_pressure(self.slip_stream_idx), \
-               (self.__get_slip_dir(self.slip_stream_idx), \
-               self.__get_slip_speed(self.slip_stream_idx))
+        return self.slip_stream_idx
 
-    def get_no_slip_stream_data(self):
+    def get_no_slip_stream_idx(self):
         """
         Return no slip data
-            validFormat: [pressure], ([slip_dir], [slip_speed])
         """
         if self.empty():
             return None
-        return self.__get_pressure(self.no_slip_stream_idx), \
-               (self.__get_slip_dir(self.no_slip_stream_idx), \
-               self.__get_slip_speed(self.no_slip_stream_idx))
+        return self.no_slip_stream_idx
 
-    def get_valid_data(self):
+    def get_valid_idx(self):
         """
         Return valid data
-            validFormat: [pressure], ([slip_dir], [slip_speed])
         """
         if self.empty():
             return None
-        return self.__get_pressure(self.valid_idx), \
-               (self.__get_slip_dir(self.valid_idx), \
-               self.__get_slip_speed(self.valid_idx))
-
+        return self.valid_idx
 
     ###########################################
     #  PRIVATE FUNCTIONS
     ###########################################
+
+    def __getitem__(self, idx):
+        """
+        Return learning data at index
+            Data Format: [pressure], ([slip_dir], [slip_speed])
+        """
+        if self.empty() or not isinstance(idx, (int, long)):
+            eprint("ERROR: Incorrect index access: {}".format(idx))
+            return None
+
+        if idx<0 or idx>=self.size():
+            eprint("ERROR: Incorrect index access: {}".format(idx))
+            return None
+
+        return self.__get_pressure(idx), \
+               (self.__get_slip_dir(idx), \
+               self.__get_slip_speed(idx))
+
+    def __len__(self):
+        return self.size()
 
     def __load_data_dir(self, data_dir, mat_format):
         print("Loading data file {}".format(data_dir))
         self.__data_dir = data_dir
         self.mat_format = mat_format
         self.__data = sio.loadmat(data_dir + "/data.mat", appendmat=mat_format)
-
+        # Check if the data contains all fields
+        if 'num' not in self.__data or \
+           'temp' not in self.__data or \
+           'pressure' not in self.__data or \
+           'time' not in self.__data or \
+           'slip_speed' not in self.__data or \
+           'slip_std' not in self.__data or \
+           'slip_dir' not in self.__data:
+            eprint("ERROR: {} has outdated data, skipping.....".format(data_dir))
+            self.__data['num'] = 0
+            return
         # calculate time period of data recording
-        if self.__data['num'] >= 2:
+        if self.size() >= 2:
             self.__data_period = self.__get_time(1) - self.__get_time(0)
 
     def __create_slip_hist(self, show=False):
@@ -173,26 +197,65 @@ class takktile_dataloader():
         self.slip_stream_idx = []    #  Slip in data[idx-len+1]->data[idx]
 
         valid_counter = 0
+        slip_counter = 0
+        no_slip_counter = 0
         # Iterate over each datapoint
-        for idx in range(self.__data['num']):
-            valid_counter += 1
+        for idx in range(self.size()):
+            if self.__is_valid(idx):
+                valid_counter += 1
+                if self.__get_slip_speed(idx) < SPEED_THRESH:
+                    no_slip_counter += 1
+                    slip_counter = 0
+                else:
+                    no_slip_counter = 0
+                    slip_counter += 1
+            else:
+                valid_counter = 0
+                no_slip_counter = 0
+                slip_counter = 0
             # data[i-s] to data[i] is the valid data for index i
             if valid_counter >= self.series_len:
                 # Storing every valid index
                 self.valid_idx.append(idx)
                 # Storing slip and non slip index
-                if self.__get_slip_speed(idx) < SPEED_THRESH:
+                if no_slip_counter > 0:
                     self.no_slip_idx.append(idx)
                 else:
                     self.slip_idx.append(idx)
-
                 # Store slip and non-slip stream indices
-                if (self.__get_slip_speed(range(idx-self.series_len+1, idx+1)) \
-                       < SPEED_THRESH).all():
+                if no_slip_counter >= self.series_len:
                     self.no_slip_stream_idx.append(idx)
-                if (self.__get_slip_speed(range(idx-self.series_len+1, idx+1)) \
-                       > SPEED_THRESH).all():
+                if slip_counter >= self.series_len:
                     self.slip_stream_idx.append(idx)
+
+    def __is_valid(self, idx):
+        """
+        Check for the current time step whether the flow vectors are in agreement
+        or not, ie, is the current measurement valid or not
+
+        Parameters
+        ----------
+        idx : int
+            The index for the data to check the validity of
+
+        Returns
+        ----------
+        valid : bool
+            Is the data at this index valid?
+        """
+        if idx<0 or idx>=self.size():
+            return False
+
+        slip_speed = self.__get_slip_speed(idx)
+        slip_vector = self.__get_slip_dir(idx) * slip_speed
+        slip_std = self.__get_slip_std(idx)
+
+        max_dim = [i for i in range(len(slip_vector)) if slip_vector[i]>SPEED_THRESH]
+
+        if (slip_std*SLIP_STD_VALID_MULTIPLIER > slip_vector)[max_dim].any():
+            return False
+
+        return True
 
     def __get_pressure(self, idx):
         return self.__data['pressure'][idx]
@@ -206,6 +269,9 @@ class takktile_dataloader():
     def __get_slip_speed(self, idx):
         return self.__data['slip_speed'][idx]
 
+    def __get_slip_std(self, idx):
+        return self.__data['slip_std'][idx]
+
     def __get_time(self, idx):
         return self.__data['time'][idx]
 
@@ -216,4 +282,6 @@ if __name__ == "__main__":
     dataloaders = []
     for dir in data_dirs:
         dataloaders.append(takktile_dataloader(dir))
-        print dataloaders[-1].save_slip_hist(location=dir+"/slip_hist.png")
+        if not dataloaders[-1].empty():
+            print("Number of datapoints that have a seq num sized trail of slip data")
+            print(len(dataloaders[-1].get_slip_idx()))
